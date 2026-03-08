9cd93a3da26d4be9bdb98ce3f0a8a236812edf7d
bhandarisiddharth256 <bhandarisiddharth256@gmail.com>
2026-03-08 23:19:33 +0530

    Add core tables: users, addresses, restaurants

---
+++ database/schema.sql
@@ -0,0 +1,49 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    
+    name TEXT NOT NULL,
+    
+    email TEXT UNIQUE NOT NULL,
+    
+    phone TEXT UNIQUE NOT NULL,
+    
+    password_hash TEXT NOT NULL,
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE addresses (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    
+    address_line TEXT NOT NULL,
+    
+    city TEXT NOT NULL,
+    
+    state TEXT NOT NULL,
+    
+    pincode TEXT NOT NULL,
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE restaurants (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    
+    name TEXT NOT NULL,
+    
+    phone TEXT UNIQUE,
+    
+    address TEXT NOT NULL,
+    
+    rating NUMERIC(2,1) DEFAULT 0,
+    
+    status restaurant_status DEFAULT 'open',
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
